30bd877e1ce98b0908e4069e526337bba25a83cf
TJ Kells <tjkells@gmail.com>
2013-02-20 00:10:53 -0600

    Add Randomly Spawning Target
    Targets now spawn on the screen at random locations while the gamestate
    is 'alive'. Clicking anywhere flips this variable to false for now.

--- main.lua
+++ main.lua
@@ -1,47 +1,84 @@
-MOAISim.openWindow ( "test", 1024, 768 )
+TARGET_SPAWN_DELAY = 5
 
-local viewport = MOAIViewport.new ()
-viewport:setSize ( 1024, 768 )
-viewport:setScale ( 1024, 768 )
+spawnCounter = TARGET_SPAWN_DELAY
+alive = true
 
-local layer = MOAILayer2D.new ()
+windowHeight = 1024
+windowWidth = 768
+MOAISim.openWindow ( "test", windowHeight, windowWidth )
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( windowHeight, windowWidth )
+viewport:setScale ( windowHeight, windowWidth )
+
+layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
 
-local layers = {}
+textLayer = MOAILayer2D.new ()
+textLayer:setViewport ( viewport )
+
+layers = {}
 table.insert(layers, layer)
+table.insert(layers, textLayer)
 MOAIRenderMgr.setRenderTable(layers)
 
-function onDraw ( index, xOff, yOff, xFlip, yFlip )
-	MOAIGfxDevice.setPenColor(1, 0.64, 0, 1)
-	MOAIDraw.fillCircle(-150,-150,50, 50)
+bholeGfx = MOAIGfxQuad2D.new ()
+bholeGfx:setTexture ( "assets/images/bhole.png" )
+bholeGfx:setRect ( -20, -20, 20, 20 )
+
+targetGfx = MOAIGfxQuad2D.new ()
+targetGfx:setTexture ( "assets/images/target.png" )
+targetGfx:setRect ( -96, -96, 96, 96 )
+
+font =  MOAIFont.new ()
+font:loadFromTTF ("assets/arialbd.ttf", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.?! ", 22, 163 )
+
+textbox = MOAITextBox.new ()
+textbox:setFont ( font )
+textbox:setRect ( -160, -80, 160, 80 )
+textbox:setLoc ( 0, 0 ) 
+textbox:setAlignment ( MOAITextBox.CENTER_JUSTIFY )
+textbox:setYFlip ( true )
+textLayer:insertProp ( textbox )
+
+local bholes = {}
+
+function setState ( prop, state, ... )
+
+	if prop then
+		if prop.finish then
+			prop:finish ()
+			prop.finish = nil
+		end
+		state ( prop, ... )
+	end
 end
 
-local scriptDeck = MOAIScriptDeck.new ()
-scriptDeck:setRect (-150,-150,50,-50)
-scriptDeck:setDrawCallback ( onDraw )
+function targetState ( prop, x, y )
 
-local prop = MOAIProp2D.new ()
-prop:setDeck ( scriptDeck )
-layer:insertProp ( prop )
+	if alive == true then
+		prop:setDeck ( targetGfx )
+		prop:setLoc ( x, y )
+		prop.state = targetState
+		layer:insertProp ( prop )
+	end
 
-function drawBhole ( x,y )
-	MOAIGfxDevice.setPenColor(1, 1, 1, 1)
-	MOAIDraw.fillCircle(x,y,15, 50)
+	bholes[prop] = true
+
+	function prop:finish ()
+		--Placeholder
+	end
+	
+	function prop:main ()
+		--Placeholder
+	end
 end
 
 function handleMouse( down )
 	if down then
-
 		local x,y = MOAIInputMgr.device.pointer:getLoc()
-		print("mouse " .. x .. " " .. y )
-
-		local bholeDeck = MOAIScriptDeck.new ()
-		bholeDeck:setRect (x, y, x + 10, y + 10)
-		bholeDeck:setDrawCallback ( drawBhole )
-
-		local bholeProp = MOAIProp2D.new ()
-		bholeProp:setDeck ( bholeDeck )
-		layer:insertProp ( bholeProp )
+		x, y = layer:wndToWorld ( x, y )
+		alive = false
 	end
 end
 MOAIInputMgr.device.mouseLeft:setCallback(handleMouse)
@@ -54,3 +91,38 @@ function handleKeyboard(key,down)
 	end
 end
 MOAIInputMgr.device.keyboard:setCallback(handleKeyboard)
+
+function main ()
+	--Main game loop
+	while alive == true do
+		
+		--Generate new randomly placed target 
+		setState ( 
+			MOAIProp2D.new()
+			, targetState
+			, math.random( 
+				((windowWidth/2) - 96) * -1
+				,((windowWidth/2) - 96)
+			)
+			, math.random( 
+				((windowHeight/2) -96) * -1
+				, (windowHeight / 2) - 96 
+			)
+		)
+
+		--Throttle thread so you dont create a million targets
+		for i = 1, 90 do
+			spawnCounter = spawnCounter + 1
+			coroutine.yield ()
+		end
+	end
+
+	while alive == false do
+		textbox:setString ( "Game Over" )
+		textbox:spool ()
+		alive = nil
+	end
+end
+
+spawnThread = MOAIThread.new ()
+spawnThread:run ( main )
